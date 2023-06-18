f2945f6c2b0284552b17a5bf7d599567e25dea68
Alexandre <santicioli@gmail.com>
2023-06-18 12:17:49 -0300

    Initial - Project created using Spring Initialzr with gradle-groovy, spring boot 3.1.0, java 17 and the following dependencies: Spring Web, Spring Data JPA, H2 Database and OpenFeign

---
+++ src/test/kotlin/br/dev/santi/patterns/PatternsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.dev.santi.patterns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PatternsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
